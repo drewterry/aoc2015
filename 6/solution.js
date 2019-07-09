26ce774722e2eb1ac2ce6b444f4eb1d0c9bd571b
Drew Terry <drew.terry@skookum.com>
2019-07-08 22:34:41 -0400

    fixed and part 2

--- 6/solution.js
+++ 6/solution.js
@@ -1,22 +1,21 @@
-// const ndarray = require('ndarray');
-// const fs = require('fs')
-
-function part1(input) {
-  const lights = new Array(1000).fill().map(() => new Array(1000).fill(false));
-
-  const commands = input.split('\n').map((line) => {
+function parseCommands(input) {
+  return input.split('\n').map((line) => {
     [begin, end] = line.match(/[0-9]{1,3},[0-9]{1,3}/g).map((coord) => coord.split(','));
 
     return {
       action: line.match(/(.*?)(?=([0-9,]))/)[0].trim(),
-      beginX: begin[0],
-      beginY: begin[1],
-      endX: end[0],
-      endY: end[1]
+      beginX: +begin[0],
+      beginY: +begin[1],
+      endX: +end[0],
+      endY: +end[1]
     };
-  })
+  });
+}
 
-  commands.forEach((command) => {
+function part1(input) {
+  const lights = new Array(1000).fill().map(() => new Array(1000).fill(false));
+
+  parseCommands(input).forEach((command) => {
     for(i=command.beginX; i <= command.endX; i++) {
       for(j=command.beginY; j <= command.endY; j++) {
         switch(command.action) {
@@ -40,15 +39,38 @@ function part1(input) {
     }, 0)
   }, 0)
 
-  // const colors = lights.reduce((res, row) => res.concat(row.map(light => light ? 255 : 0)), []);
-  // const lightsNd = ndarray(colors, [1000, 1000]);
-  // require("save-pixels")(lightsNd, 'png').pipe(fs.createWriteStream("myOutput.png"))
-
   return count;
 }
 
 function part2(input) {
-  return false
+  const lights = new Array(1000).fill().map(() => new Array(1000).fill(0));
+
+  parseCommands(input).forEach((command) => {
+    for(i=command.beginX; i <= command.endX; i++) {
+      for(j=command.beginY; j <= command.endY; j++) {
+        switch(command.action) {
+          case 'turn on':
+            lights[i][j] = lights[i][j] + 1;
+            break;
+          case 'toggle':
+            lights[i][j] = lights[i][j] + 2;
+            break;
+          case 'turn off':
+            lights[i][j] = lights[i][j] - 1;
+            break;
+        }
+        lights[i][j] = Math.max(lights[i][j], 0);
+      }
+    }
+  })
+  
+  const count = lights.reduce((count, row) => {
+    return count + row.reduce((count, light) => {
+      return count + light;
+    }, 0)
+  }, 0)
+
+  return count;
 }
 
 module.exports = {
--- 6/test.js
+++ 6/test.js
@@ -22,4 +22,29 @@ describe('part1', () => {
     
     expect(solution.part1(input)).toBe(998996);
   });
+
+  test('toggle 0,999 through 999,999', () => {
+    input = `turn on 0,0 through 999,999
+    toggle 0,0 through 999,0
+    toggle 0,999 through 999,999
+    turn off 499,499 through 500,500`;
+
+    expect(solution.part1(input)).toBe(997996);
+  });
 });
+
+describe('part2', () => {
+
+  test('turn on 0,0 through 0,0', () => {
+    input = `turn on 0,0 through 0,0`;
+
+    expect(solution.part2(input)).toBe(1);
+  });
+
+  test('toggle 0,0 through 999,999', () => {
+    input = `toggle 0,0 through 999,999`;
+
+    expect(solution.part2(input)).toBe(2000000);
+  });
+});
+
